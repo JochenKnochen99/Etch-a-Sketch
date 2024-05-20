ba9ddc57163ddca9f4dce2730e32fff9bfe94bfc
JochenKnochen99 <167319019+JochenKnochen99@users.noreply.github.com>
2024-05-20 18:26:44 +0200

    update JS now every div 'pixel' change colors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const div = document.querySelector(".container");
-let gridDiv;
 
 function divCreate (){
     for (let i = 0; i < 256; i++){
-        gridDiv = document.createElement("div");
+        let gridDiv = document.createElement("div");
         gridDiv.style.cssText = "display: flex; border: 1px solid blue; flex-grow: 1; flex-shrink: 1; min-height: 60px; min-width: 60px; flex-wrap: wrap;";
+        
         div.appendChild(gridDiv);
+
+        gridDiv.addEventListener("mouseenter", (event) => {
+            event.stopPropagation();
+            let number1 = Math.floor(Math.random() * 256);
+            let number2 = Math.floor(Math.random() * 256);
+            let number3 = Math.floor(Math.random() * 256);
+            gridDiv.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
+        });
     }
 }
 
@@ -16,10 +24,4 @@ function divCreate (){
 */
  divCreate();
 
- gridDiv.addEventListener("mouseenter", (event) => {
-    event.stopPropagation();
-    let number1 = Math.floor(Math.random() * 256);
-    let number2 = Math.floor(Math.random() * 256);
-    let number3 = Math.floor(Math.random() * 256);
-    gridDiv.style.cssText += `background-color: rgb(${number1}, ${number2}, ${number3})`;
- });
+ 
