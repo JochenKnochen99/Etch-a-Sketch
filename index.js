161718d8756dd36bc5439a71e8a75c74efadfe7f
JochenKnochen99 <167319019+JochenKnochen99@users.noreply.github.com>
2024-05-21 18:11:59 +0200

    Add functionality user can now resize grid with button

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+const body = document.querySelector("body")
 const div = document.querySelector(".container");
-let userInput = Number(window.prompt("Please enter a number between 2 and 100 for you Grid Size"));
+const btn = document.createElement("button");
+btn.style.cssText = "color: red; background-color: black; font-weight: bold; font-size: 25px; width: 250px; height: 50px; margin: 50px; border-radius: 25px; cursor: pointer;";
+btn.innerText = "Resize Grid";
+body.appendChild(btn);
 
-function divCreate (size){
-    if(size >= 2 || size <= 100){
+
+function divCreate (){
+    
+
+    for (let i = 0; i < 256; i++){
+        let gridDiv = document.createElement("div");
+        gridDiv.style.cssText = `display: flex;  flex-grow: 1; flex-shrink: 1; min-height: 60px; min-width: 60px; flex-wrap: wrap;`;
+        
+        div.appendChild(gridDiv);
+
+        gridDiv.addEventListener("mouseenter", (event) => {
+            event.stopPropagation();
+            let number1 = Math.floor(Math.random() * 256);
+            let number2 = Math.floor(Math.random() * 256);
+            let number3 = Math.floor(Math.random() * 256);
+            gridDiv.style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
+        });
+    }
+    } 
+
+
+
+ 
+
+
+ function divCreateCustomGrid (){
+    
+    let size = Number(window.prompt("Please enter a number between 2 and 100 for you Grid Size"));
+
+    if(size >= 2 && size <= 100){
     let gridSize = (size * size);
     let divSize = (960 / size);
-
+    
+   
     for (let i = 0; i < gridSize; i++){
         let gridDiv = document.createElement("div");
         gridDiv.style.cssText = `display: flex;  flex-grow: 1; flex-shrink: 1; min-height: ${divSize}px; min-width: ${divSize}px; flex-wrap: wrap;`;
@@ -26,14 +59,22 @@ function divCreate (size){
 
 }
 
-/*
-nehme user input
-loop condition (i) = input * input 
-div größe = 960 / input (max 100)
-min heigt ${div größe} | min-width ${div größe}
+function deleteChild() {
+    let e = document.querySelector("div"); 
+    let child = e.lastElementChild;
+
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    }
+}
 
 
-divCreate(user Input)
-*/
- divCreate(userInput);
 
+btn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    deleteChild();
+    divCreateCustomGrid();
+} );
+
+divCreate();
